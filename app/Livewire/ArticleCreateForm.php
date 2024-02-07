80ac53d1a2100c65fd59976f3d0921da16c0c133
DavideCariola <davide.cariola@aulab.it>
2024-02-07 16:44:38 +0200

    completata article.store

--- app/Livewire/ArticleCreateForm.php
+++ app/Livewire/ArticleCreateForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Article;
 use Livewire\Component;
 
 class ArticleCreateForm extends Component
@@ -9,6 +10,27 @@ class ArticleCreateForm extends Component
     public $title;
     public $subtitle;
     public $content;
+    protected $GOOGLE_KEY = 1234567890;
+
+    public function store()
+    {
+        Article::create([
+            'title' => $this->title,
+            'subtitle' => $this->subtitle,
+            'content' => $this->content,
+        ]);
+
+        $this->cleanForm();
+
+        session()->flash('message', 'Articolo creato con successo.');
+    }
+
+    protected function cleanForm()
+    {
+        $this->title = '';
+        $this->subtitle = '';
+        $this->content = '';
+    }
 
     public function render()
     {
--- resources/views/livewire/article-create-form.blade.php
+++ resources/views/livewire/article-create-form.blade.php
@@ -1,17 +1,25 @@
-<form class="border rounded p-5">
+<form class="border rounded p-5" wire:submit="store">
+
+    @if(session('message'))
+        <div class="alert alert-success text-center">
+            {{ session('message') }}
+        </div>
+    @endif
+
     <div class="mb-3">
         <label for="title" class="form-label">Titolo articolo</label>
-        <input type="text" class="form-control" id="title">
+        <span class="text-danger">Il mio titolo e' {{ $title }}</span>
+        <input type="text" wire:model.live="title" class="form-control" id="title">
     </div>
   
     <div class="mb-3">
         <label for="subtitle" class="form-label">Sottotitolo articolo</label>
-        <input type="text" class="form-control" id="subtitle">
+        <input type="text" wire:model.live="subtitle" class="form-control" id="subtitle">
     </div>
   
     <div class="mb-5">
         <label class="form-label" for="content">Corpo del testo</label>
-        <textarea id="content" cols="30" rows="10" class="form-control"></textarea>
+        <textarea wire:model.live="content" id="content" cols="30" rows="10" class="form-control"></textarea>
     </div>
   
     <div class="d-flex justify-content-end">
